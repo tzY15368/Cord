32a9656d49f803c4f138f3b51fc86ab3fdb67a0c
Nathan <tzy15368@outlook.com>
2022-03-18 12:19:45 +0800

    change raft heartbeat interval

--- src/raft/common.go
+++ src/raft/common.go
@@ -4,7 +4,7 @@ import "time"
 
 const (
 	electionTimeout  = time.Millisecond * 300
-	hearbeatInterval = time.Millisecond * 150
+	hearbeatInterval = time.Millisecond * 100
 )
 
 func (rf *Raft) panicHandler() {
