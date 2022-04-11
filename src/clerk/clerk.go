dcabfe026d74fa875fca154af06121597864d07a
Nathan <tzy15368@outlook.com>
2022-04-11 15:46:44 +0800

    add clerk sleep

--- src/clerk/clerk.go
+++ src/clerk/clerk.go
@@ -3,15 +3,20 @@ package clerk
 import (
 	"context"
 	"sync/atomic"
+	"time"
 
 	"6.824/common"
+	"6.824/logging"
 	"6.824/proto"
+	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
 
 type Clerk struct {
 	localRequestInfo *proto.RequestInfo
 	conns            []*grpc.ClientConn
+	addrOffset       int32
+	logger           *logrus.Logger
 }
 
 func NewClerk(addrs []string) *Clerk {
@@ -20,7 +25,9 @@ func NewClerk(addrs []string) *Clerk {
 			ClientID:  common.Nrand(),
 			RequestID: 0,
 		},
-		conns: make([]*grpc.ClientConn, len(addrs)),
+		conns:      make([]*grpc.ClientConn, len(addrs)),
+		addrOffset: 0,
+		logger:     logging.GetLogger("clerk", logrus.DebugLevel),
 	}
 	var err error
 	for i := range addrs {
@@ -33,8 +40,17 @@ func NewClerk(addrs []string) *Clerk {
 }
 
 func (c *Clerk) HandleRequest(ctx context.Context, args *proto.ServiceArgs) (*proto.ServiceReply, error) {
-	cli := proto.NewExternalServiceClient(c.conns[0])
-	return cli.HandleRequest(ctx, args)
+retry:
+	currentOffset := atomic.LoadInt32(&c.addrOffset) % int32(len(c.conns))
+	cli := proto.NewExternalServiceClient(c.conns[currentOffset])
+	reply, err := cli.HandleRequest(ctx, args)
+	if err != nil {
+		logrus.WithError(err).WithField("currentOffset", currentOffset).Warn("request failed, sleeping 500ms")
+		atomic.AddInt32(&c.addrOffset, 1)
+		time.Sleep(500 * time.Millisecond)
+		goto retry
+	}
+	return reply, nil
 }
 
 func (cs *Clerk) CreateRequestInfo() proto.RequestInfo {
