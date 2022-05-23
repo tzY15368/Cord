36825b0c9e1e06d351af0ce327bfd0ef6ebea24f
Nathan <tzy15368@outlook.com>
2022-05-23 13:43:57 +0800

    up

--- 实验数据/encoding.py
+++ 实验数据/encoding.py
@@ -1,4 +1,12 @@
 import matplotlib.pyplot as plt
+
+
+################
+merge = False
+##############
+
+
+
 plt.rcParams["font.sans-serif"]=["SimHei"] #设置字体
 plt.rcParams["axes.unicode_minus"]=False #该语句解决图像中的“-”负号的乱码问题
 _times = [1998,21.23]
@@ -15,7 +23,8 @@ plt.figure(figsize=(6,6))
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
 b1  = ax1.bar(x1,_times,label='每次操作耗时（ns)',width=width,color="orange")
-ax1.set_xlabel('（1）序列化库性能对比')
+if merge:
+    ax1.set_xlabel('（1）序列化库性能对比')
 
 b2 = ax2.bar(x2,_alloc,label='每次操作内存分配（次）',width=width)
 
@@ -33,7 +42,8 @@ plt.figure(figsize=(6,6))
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
 b1  = ax1.bar(x1,_times,label='每次操作耗时（ns)',width=width,color="orange")
-ax1.set_xlabel('（2）状态持久化底层依赖性能对比')
+if merge:
+    ax1.set_xlabel('（2）状态持久化底层依赖性能对比')
 
 b2 = ax2.bar(x2,_alloc,label='每次操作内存分配（次）',width=width)
 
@@ -46,10 +56,13 @@ def merge_image(i1:str,i2:str,outp:str):
     img1 = Image.open(i1)
 
     img2 = Image.open(i2)
-
     out = Image.new('RGB',(img2.size[0]+img1.size[0],img1.size[1])) 
     out.paste(img1) 
     out.paste(img2,(img1.size[0],0))
     out.save(outp,quality=100)
+
+if not merge:
+    exit(0)
+
 
 merge_image('gob.jpg','mmap.jpg','encoding-out.jpg')
